f47fa7f7b656779bf4ff02723f30c6ce61b051b4
Dinesh Sarikonda <sarikondadinesh23@gmail.com>
2025-04-29 12:33:19 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setyptools import find_packages, setup
+
+set(
+    name = 'GenAIproject',
+    version= '0.0.0',
+    author= 'Dinesh',
+    author_email='sarikondadinesh23@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
